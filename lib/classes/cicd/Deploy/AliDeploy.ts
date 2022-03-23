c2e34a98137596cc52d6d20aec435c3e00f53c2f
dinghongyu <dinghongyu@rys.com>
2022-03-23 15:57:36 +0800

    add Expires Header

--- lib/classes/cicd/Deploy/AliDeploy.ts
+++ lib/classes/cicd/Deploy/AliDeploy.ts
@@ -1,78 +1,76 @@
 import aliOSS from 'ali-oss';
 import fs from 'fs';
-import {DeployTarget} from '../CICD';
+import { DeployTarget } from '../CICD';
 import os from 'os';
-import {cat} from 'shelljs';
 
 class AliOSS {
-	ossClient: aliOSS;
+  ossClient: aliOSS;
 
-	constructor(target: DeployTarget) {
-		this.ossClient = new aliOSS({
-			region: target.region,
-			accessKeyId: target.access_key,
-			accessKeySecret: target.access_secret,
-			bucket: target.bucket,
-			timeout: 600000,
-		});
-	}
+  constructor(target: DeployTarget) {
+    this.ossClient = new aliOSS({
+      region: target.region,
+      accessKeyId: target.access_key,
+      accessKeySecret: target.access_secret,
+      bucket: target.bucket,
+      timeout: 600000,
+    });
+  }
 
-	private async progress(p: number, filePath: string, ossPath: string) {
-		// 上传进度。
-		try {
-			process.stdout.clearLine(1);
-			process.stdout.cursorTo(0);
-		} catch (e) {
-
-		}
-		try{
+  private async progress(p: number, filePath: string, ossPath: string) {
+    // 上传进度。
+    try {
+      process.stdout.clearLine(1);
+      process.stdout.cursorTo(0);
+    } catch (e) {}
+    try {
       process.stdout.write(
-        `progress: ${p.toFixed(2)}%, Upload '${filePath}' To OSS_PATH:${ossPath}`
+        `progress: ${p.toFixed(
+          2
+        )}%, Upload '${filePath}' To OSS_PATH:${ossPath}`
       );
-    }catch (e) {
-      
+    } catch (e) {}
+  }
+
+  public async putStreamFiles(
+    filesList: string[],
+    ossBasePath: string,
+    dir: string,
+    rootPath: string
+  ) {
+    for (let i = 0; i < filesList.length; i++) {
+      let filePath = '';
+      if (os.platform() === 'win32') {
+        filePath = filesList[i].split(`${rootPath}\\`)[1];
+      } else {
+        filePath = filesList[i].split(`${rootPath}/`)[1];
+      }
+      const ossPath =
+        ossBasePath + filePath.replace(/\\/g, '/').replace(dir, '');
+
+      //@ts-ignore
+      let options: aliOSS.PutStreamOptions = {
+        contentLength: fs.statSync(filesList[i]).size,
+      };
+      if (filesList[i].includes('index.html')) {
+        options = Object.assign({
+          headers: { 'Cache-Control': 'max-age=0', Expires: -1 },
+        });
+      }
+      const fileResult = await this.ossClient.putStream(
+        ossPath,
+        fs.createReadStream(filesList[i]),
+        options
+      );
+      if (fileResult.res.status !== 200) {
+        throw new Error('Upload OSS Error');
+      }
+      if (fileResult.res.status === 200) {
+        const p = ((i + 1) * 100) / filesList.length;
+        this.progress(p, filesList[i], ossPath);
+      }
     }
-
-	}
-
-	public async putStreamFiles(
-		filesList: string[],
-		ossBasePath: string,
-		dir: string,
-		rootPath: string
-	) {
-		for (let i = 0; i < filesList.length; i++) {
-			let filePath = '';
-			if (os.platform() === 'win32') {
-				filePath = filesList[i].split(`${rootPath}\\`)[1];
-			} else {
-				filePath = filesList[i].split(`${rootPath}/`)[1];
-			}
-			const ossPath =
-				ossBasePath + filePath.replace(/\\/g, '/').replace(dir, '');
-
-			//@ts-ignore
-			let options: aliOSS.PutStreamOptions = {
-				contentLength: fs.statSync(filesList[i]).size,
-			};
-			if (filesList[i].includes('index.html')) {
-				options = Object.assign({headers: {'Cache-Control': 'max-age=0'}});
-			}
-			const fileResult = await this.ossClient.putStream(
-				ossPath,
-				fs.createReadStream(filesList[i]),
-				options
-			);
-			if (fileResult.res.status !== 200) {
-				throw new Error('Upload OSS Error');
-			}
-			if (fileResult.res.status === 200) {
-				const p = ((i + 1) * 100) / filesList.length;
-				this.progress(p, filesList[i], ossPath);
-			}
-		}
-		console.log('\n');
-	}
+    console.log('\n');
+  }
 }
 
 export default AliOSS;
